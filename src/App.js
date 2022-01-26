bfbb4162a2e092a13235268488a1fd5c0480dc26
partnadem <team@forasoft.com>
2022-01-26 19:55:25 +0300

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import background from './assets/mm.jpg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <AppBackground backgroundPath={background} />
+      <GameFrame>Wake up</GameFrame>
+    </AppWrapper>
   );
-}
+};
+
+const AppWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  background-color: #000;
+`;
+
+const AppBackground = styled.div`
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  background: url(${(props) => props.backgroundPath}) #000;
+  opacity: 0.07;
+`;
+
+const GameFrame = styled.div`
+  display: flex;
+  height: 600px;
+  max-width: 400px;
+  min-width: 300px;
+  box-sizing: border-box;
+  border: 2px solid #fff;
+  color: #fff;
+`;
 
 export default App;
